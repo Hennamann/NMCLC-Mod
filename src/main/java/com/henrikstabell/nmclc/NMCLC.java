5b5029161ee4492462582b5526be36ddc0f84c56
Ole Henrik Stabell <ole@henrikstabell.com>
2018-03-08 12:23:32 +0100

    Added optional support for servers, updated to latest forge ver.

--- src/main/java/com/henrikstabell/nmclc/NMCLC.java
+++ src/main/java/com/henrikstabell/nmclc/NMCLC.java
@@ -1,11 +1,9 @@
 package com.henrikstabell.nmclc;
 
 import com.henrikstabell.nmclc.handler.ChatHandler;
-import net.minecraft.init.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -14,7 +12,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class NMCLC
 {
     public static final String MODID = "nmclc";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.2.0";
     public static final String NAME = "NMCLC (NoMoreCapsLockCommands)";
 
     @SideOnly(Side.CLIENT)
