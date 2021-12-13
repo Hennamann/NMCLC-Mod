ff52e3358a9dd42a007f677d92e0ded52ed58350
Ole Henrik Stabell <ole@henrikstabell.com>
2021-12-13 14:35:25 +0100

    Update to 1.18.1

--- src/main/java/com/henrikstabell/nmclc/NMCLC.java
+++ src/main/java/com/henrikstabell/nmclc/NMCLC.java
@@ -2,6 +2,7 @@ package com.henrikstabell.nmclc;
 
 import net.minecraftforge.client.event.ClientChatEvent;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -9,7 +10,6 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fmlserverevents.FMLServerStartingEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -43,7 +43,7 @@ public class NMCLC {
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
-    public void onServerStarting(FMLServerStartingEvent event) {}
+    public void onServerStarting(ServerStartingEvent event) {}
 
     @SubscribeEvent
     public void onGetChatMessage(ClientChatEvent event) {
